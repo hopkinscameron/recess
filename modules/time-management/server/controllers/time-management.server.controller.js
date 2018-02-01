89b26be79b036ad446fd904345b38a8cd0b9e8d9
Cameron Hopkins <hopkins_cameron@yahoo.com>
2018-02-01 12:10:09 -0500

    Quick bug fix with user types

--- modules/time-management/server/controllers/time-management.server.controller.js
+++ modules/time-management/server/controllers/time-management.server.controller.js
@@ -472,7 +472,7 @@ function getUserTimeManagement(user, todayOnly) {
                 }
             }
             else {
-                resolve({});
+                resolve([]);
             }
         });
     });
